44d02a3371746fc12facac082d3fb6867c2a28ae
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-01-04 18:20:37 +0100

    Modify app engine yaml file and config

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
+"""
+WSGI config for backend project.
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+sys.path.insert(0, '/Users/robertoalvarez/Desktop/VirtualEnv/News_refactor/lib/python2.7/site-packages')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.backend.settings")
+MakeMigration = False
+
+import django.core.handlers.wsgi
+django.setup()
+
+application = django.core.handlers.wsgi.WSGIHandler()
