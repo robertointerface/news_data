6ecdc361e626ec238d034f95455e676b6ca7b17d
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-02-12 15:22:29 +0100

    Specify 'username' as the query field on relational field 'created_by' on serializer 'NewSerializer'

--- backend/create_new/serializers.py
+++ backend/create_new/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import New
 
 class NewSerializer(serializers.ModelSerializer):
-
+    created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
     class Meta:
         model = New
         fields = '__all__'
