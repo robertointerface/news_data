87accfd19876903bef065bbab416e974bc77661c
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-02-13 19:26:40 +0100

    Modify function initializeUserManagement, token needs to be refreshed only after the user data saved in localstorage is passed to User_management reducer.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -23,23 +23,28 @@ import promiseMiddleware from 'redux-promise';
 import {reducer as formReducer} from 'redux-form'
 import {handle_refresh_token} from 'functions/auth/LoginFunctions'
 const initializeUserManagement = () => {
+    /*
+        @Func: Check if a user logged in previously (user data is saved on localStorage),
+        if user logged in previously, the User_management state will be populated with the corresponding user data and
+        the JWT (Json Web Token) will be refreshed if possible, if JWT already expired and can not be
+        verified it will be deleted from localStorage and user will be forced to logged in again.
 
+     */
     var userObject = {...userData};
     var userSaved = JSON.parse(localStorage.getItem('user'));
     if(userSaved){
+        userObject['username'] = userSaved['username'];
+        userObject['email'] = userSaved['email'];
+        userObject['logged_in'] = true;
+
         handle_refresh_token()
-            .then(token => {
-                localStorage.setItem('token', token)
-                userObject['username'] = userSaved['username'];
-                userObject['email'] = userSaved['email'];
-                userObject['logged_in'] = true;
-                return userObject
-            })
-            .catch(error => {
-                console.log('error: ' + error)
-                localStorage.clear();
-                return userObject
-            })
+        .then(token => {
+            localStorage.setItem('token', token)
+        })
+        .catch(error => {
+            console.log('error: ' + error)
+            localStorage.clear();
+        })
     }
     return userObject
 }
