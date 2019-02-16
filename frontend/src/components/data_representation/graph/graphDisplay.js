faa5c1e76f8bdd9352d59fad744e6ab176399960
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-02-16 11:06:04 +0100

    Implement remove button to each 'GraphDisplay' component, no functionality yet.

--- frontend/src/components/data_representation/graph/graphDisplay.js
+++ frontend/src/components/data_representation/graph/graphDisplay.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
-
 import {Line} from 'react-chartjs-2';
-
-
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 class GraphDisplay extends Component{
@@ -15,10 +14,21 @@ class GraphDisplay extends Component{
             <div className='row'>
                 {(list.length > 0) ?
                     list.map((chart, i) =>
-                    <div className='col-6' key={`CHART-${chart.id}-${i}`}>
-                        <Line data={chart.data} options={chart.options} width="600" height="400"/>
-                        <p>{chart.explanation}</p>
-                    </div>
+                        <div className='card col-6'>
+                            <div className='row'>
+                                <div className='col-10'>
+                                    <p>{chart.explanation}</p>
+                                </div>
+                                <div className='col-2'>
+                                     <a href='#'>
+                                        <FontAwesomeIcon className='F12' icon={faTimesCircle}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div key={`CHART-${chart.id}-${i}`}>
+                                <Line data={chart.data} options={chart.options} width="600" height="400"/>
+                            </div>
+                        </div>
                     )
                     :null
                 }
--- frontend/src/containers/createNewContainers.js
+++ frontend/src/containers/createNewContainers.js
@@ -76,6 +76,7 @@ export const GraphDisplayContainer = connect (
     dispatch =>
         ({
 
+
         })
 )(GraphDisplay)
 
