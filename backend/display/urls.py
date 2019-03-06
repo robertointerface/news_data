0538a36ee08c6296f72589ece1b18011668b5aec
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-03-06 10:06:52 +0100

    fix typo in url

--- backend/display/urls.py
+++ backend/display/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import url, include
 
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 import views
 
 urlpatterns = [
         url(r'^newslist/$', views.GetNewList.as_view()),
         url(r'^getnew/$', views.GetNew.as_view()),
         url(r'^getnewscount/$', views.GetNewsCount.as_view()),
-        url(r'^usernews/$', views.GetUserPublishedNews.as_view())
+        url(r'^usernews$', views.GetUserPublishedNews.as_view()),
+
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
