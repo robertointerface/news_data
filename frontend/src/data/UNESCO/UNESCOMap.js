f16f1638da9a06dc00a8de98b36dc69c084e1f5f
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-03-29 12:57:12 +0100

    Delete unused data and move 'DisplayExtraMessage' to correct location

--- frontend/src/data/UNESCO/UNESCOMap.js
+++ frontend/src/data/UNESCO/UNESCOMap.js
@@ -24,7 +24,6 @@ const UnescoDatabase = [
 				    	orderOption: [C.TOPIC, C.INDICATOR, ['_T', '_T'] , C.GEO],
 				    	IndicatorName: "UNIT_MEASURE",
 				    	DisplayMessageOrder:['Indicator'],
-				 		DisplayExtra: ''
 
 					}
 				},
@@ -44,8 +43,7 @@ const UnescoDatabase = [
 				     UrlStructure:{
 				    	orderOption: [C.TOPIC, C.INDICATOR, '_T', 'Y0T1', C.GEO],
 				    	IndicatorName: "UNIT_MEASURE",
-				    	DisplayMessageOrder:['Extra','Indicator'],
-				 		DisplayExtra: ' children deaths under one year of age ',
+				    	DisplayMessageOrder:['children deaths under one year of age','Indicator'],
 					}
 				},
 
@@ -88,7 +86,7 @@ const UnescoDatabase = [
 				    	orderOption: [C.TOPIC, C.INDICATOR, ['_T', '_T'], C.GEO],
 				    	IndicatorName: "UNIT_MEASURE",
 				    	DisplayMessageOrder:['Indicator'],
-				 		DisplayExtra: '',
+
 
 					}
 				},
@@ -109,8 +107,7 @@ const UnescoDatabase = [
 
 						orderOption:[C.TOPIC, C.INDICATOR, ['_T', '_T'], C.GEO],
 						IndicatorName: "UNIT_MEASURE",
-						DisplayMessageOrder:['Extra','Indicator'],
-				 		DisplayExtra: ' Population living in rural areas ',
+						DisplayMessageOrder:['Population living in rural area','Indicator'],
 
 					}
 				},
@@ -132,8 +129,7 @@ const UnescoDatabase = [
 				    	orderOption: [C.TOPIC, C.MEASURE, '_T', C.INDICATOR, C.GEO],
 				    	IndicatorName: "AGE",
 					    UnitName: 'UNIT_MEASURE',
-					    DisplayMessageOrder:['Unit','Extra','Indicator'],
-				 		DisplayExtra: ' with ages ',
+					    DisplayMessageOrder:['Unit','with ages','Indicator'],
 
 					}
 				},
@@ -199,8 +195,7 @@ const UnescoDatabase = [
 				     UrlStructure:{
 				    	orderOption: [C.TOPIC, C.INDICATOR, ['_Z', '_Z'], C.GEO],
 				    	IndicatorName: "UNIT_MEASURE",
-				    	DisplayMessageOrder:['Extra','Indicator'],
-				 		DisplayExtra: ' GDP per capita at ',
+				    	DisplayMessageOrder:[' GDP per capita at','Indicator'],
 					}
 				},
 			/*	{
@@ -267,137 +262,6 @@ const UnescoDatabase = [
 
 	   ]
     },
-   /* {
-   	   SectorName: "Education",
-   	   id: 'EDU_NON_FINANCE-Rev1',
-   	   Topics:[
-					{
-						TopicName: "Net enrolment rate by education level",
-					    id: 'NER',
-					    rev: 1,
-						Geo: {
-							location: 'UnescoEduRegions'
-						},
-					    Time: {
-					     	Type: 'A',
-					     	Start: 1990,
-					      	End: 2015
-					      },
-					     UrlStructure:{
-							orderOption: [C.TOPIC, C.MEASURE, C.INDICATOR, ['_T', '_T','SCH_AGE_GROUP','_T',
-								'INST_T', '_Z', '_Z', '_T' , '_T', '_T', '_Z', '_Z', '_Z', '_Z', '_Z', 'W00', 'W00','_Z' ], C.GEO],
-					        IndicatorName: "EDU_LEVEL",
-					        UnitName: 'UNIT_MEASURE',
-					        DisplayMessageOrder:['Unit' ,'Indicator' ],
-					 		DisplayExtra: '',
-
-						}
-					},
-					{
-						TopicName: "Literacy rate",
-					    id: 'LR',
-					    rev: 1,
-					    Geo: {
-							location: 'UnescoEduRegions'
-						},
-
-					    Time: {
-					     	Type: 'A',
-					     	Start: 1985,
-					      	End: 2015
-					      },
-					     UrlStructure:{
-					        orderOption: ['D', 'U', 'E', {'I': 4}],
-					        IndicatorName: "AGE",
-					        UnitName: 'UNIT_MEASURE',
-					        DisplayMessageOrder:['Unit' ,'Indicator', 'Extra' ],
-					 		DisplayExtra: ' that know how to read',
-					 		extras: ['PT', '_Z', '_Z','_T','_Z','_Z', '_Z', '_Z',
-					 			       '_T' , '_Z', '_Z', '_Z', '_Z', 'W00', 'W00' ]
-
-						}
-					},
-
-					{
-						TopicName: "% students that are women",
-					    id: 'FEP',
-					    rev: 1,
-					    Geo: {
-							location: 'UnescoEduRegions'
-						},
-
-					    Time: {
-					     	Type: 'A',
-					     	Start: 2000,
-					      	End: 2015
-					      },
-					     UrlStructure:{
-					        orderOption: 1,
-					        IndicatorPosition: 1,
-					        IndicatorName: "EDU_LEVEL",
-					        UnitName: 'UNIT_MEASURE',
-					        DisplayMessageOrder:['Unit' , 'Indicator',  'Extra'],
-					 		DisplayExtra: ' that are women ',
-					 		extras:['PT', '_T', 'F','_T','_T','INST_T', '', '',
-					 			       '_T' , '_T', '_T', '', '', 'W00', 'W00' ]
-
-						}
-					},
-
-					{
-						TopicName: "Repetition rate",
-					    id: 'REPP',
-					    rev: 1,
-					    Geo: {
-							location: 'Unesco'
-						},
-
-					    Time: {
-					     	Type: 'A',
-					     	Start: 2000,
-					      	End: 2015
-					      },
-					     UrlStructure:{
-					        orderOption: 1,
-					        IndicatorPosition: 1,
-					        IndicatorName: "EDU_LEVEL",
-					        UnitName: 'UNIT_MEASURE',
-					        DisplayMessageOrder:['Unit' , 'Indicator',  'Extra'],
-					 		DisplayExtra: ' that repeat one course or more',
-					 		extras: ['PT', '' ,'_T','_T','_T','INST_T', '', '',
-					 			       '_T' , 'INIT', '_T', '', '', 'W00', 'W00' ]
-
-						}
-					},
-
-					{
-						TopicName: "Duration of compulsory education",
-					    id: 'TH_DUR_COMP_EDU',
-					    rev: 1,
-					     Geo: {
-							location: 'Unesco'
-						},
-
-					    Time: {
-					     	Type: 'A',
-					     	Start: 2000,
-					      	End: 2015
-					      },
-					     UrlStructure:{
-					        orderOption: 1,
-					        IndicatorPosition: 0,
-					        IndicatorName: "UNIT_MEASURE",
-					        DisplayMessageOrder:['Indicator', 'Extra'],
-					 		DisplayExtra: ' required to finish compulsory education',
-					 		extras: ['_T', '_T' ,'_T','_T','_T','INST_T', '', '',
-					 			       '_T' , '_T', '_T', '', '', 'W00', 'W00' ]
-
-						}
-					},
-
-
-   	              ]
-    },*/
     {
     	   SectorName: "Education expenditure",
     	   id: 'EDU_FINANCE-Rev1',
@@ -495,256 +359,6 @@ const UnescoDatabase = [
  	               ]
 
     },
-
-
-
-   /* {
-   	   SectorName: "Cinema industry",
-   	   id: 'FF-Rev1',
-   	   Topics:[
-			{
-				TopicName: "Produced films by type of film",
-			    id: 'FF',
-			    rev: 1,
-			     Geo: {
-					location: 'Unesco'
-				 },
-
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2017
-			      },
-			     UrlStructure:{
-					orderOption: [C.MEASURE, C.TOPIC, C.GEO, ['_T','NAT', '_T', '_T', '_T',], C.INDICATOR, ['PROD', '_T']],
-			        IndicatorName: 'FILM_TYPE',
-			        UnitName: 'UNIT_MEASURE',
-			        DisplayMessageOrder:['Unit' , 'Indicator', 'produced' ],
-
-				}
-			},
-			{
-				TopicName: "Produced films by type of financing",
-			    id: 'FF',
-			    rev: 2,
-			   Geo: {
-					location: 'Unesco'
-				 },
-
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-			        UnitMessurePosition: 0,
-			        IndicatorPosition: 5,
-			        CountryPosition: 2,
-			        UnitName: 'UNIT_MEASURE',
-			        IndicatorName: 'FINANCE_TYPE',
-			        DisplayMessageOrder:['Unit' , 'Extra', 'Indicator' ],
-			 		DisplayExtra: ' of films ',
-			 		extras: [ '_T','NAT', '_T', '_T', '_T',
-			 			         'PROD', '_T']
-
-				}
-			},
-			{
-				TopicName: "Number of Cinemas by size",
-			    id: 'C_SITE',
-			    rev: 1,
-			   Geo: {
-					location: 'Unesco'
-				 },
-
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-			        UnitMessurePosition: 0,
-			        IndicatorPosition: 1,
-			        CountryPosition: 2,
-			        UnitName: 'UNIT_MEASURE',
-			        IndicatorName: 'NB_SCREEN',
-			        DisplayMessageOrder:['Unit' , 'Extra', 'Indicator' ],
-			 		DisplayExtra: ' of ',
-			 		extras: [ '_T', '_T', '_T',
-			 			       '_T', '_T', 'INFRA', 'C_SITE_INDOOR']
-
-				}
-			},
-
-			{
-				TopicName: "Feature film",
-			    id: 'FF',
-			    rev: 3,
-			   	Geo: {
-					location: 'Unesco'
-				 },
-
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-			        UnitMessurePosition: 0,
-			        IndicatorPosition: 2,
-			        CountryPosition: 2,
-			        UnitName: 'UNIT_MEASURE',
-			        IndicatorName: 'ORIGIN',
-			        DisplayMessageOrder:['Unit' , 'Extra', 'Indicator' ],
-			 		DisplayExtra: ' of ',
-			 		extras: [ '_T', '_T', '_T',
-			 			       '_T', '_T', 'EXHIB', '_T']
-
-				}
-			},
-			{
-				TopicName: "Cinema Admissions",
-			    id: 'ADM',
-			    rev: 1,
-			   	Geo: {
-					location: 'Unesco'
-				 },
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-			        UnitMessurePosition: 0,
-			        IndicatorPosition: 2,
-			        CountryPosition: 2,
-			        UnitName: 'UNIT_MEASURE',
-			        IndicatorName: 'ORIGIN',
-			        DisplayMessageOrder:['Unit' , 'Extra', 'Indicator' ],
-			 		DisplayExtra: ' of admissions for ',
-			 		extras: [ '_T', '_T', '_T',
-							  '_T', '_T', 'EXHIB', '_T']
-
-				}
-			},
-			{
-				TopicName: "Gross box office",
-			    id: 'GBO',
-			    rev: 1,
-			   	Geo: {
-					location: 'Unesco'
-				 },
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-			        UnitMessurePosition: 0,
-			        IndicatorPosition: 2,
-			        CountryPosition: 2,
-			        UnitName: 'UNIT_MEASURE',
-			        IndicatorName: 'ORIGIN',
-			        DisplayMessageOrder:['Indicator', 'Extra', 'Unit'],
-			 		DisplayExtra: ' Gross box office in ',
-			 		extras: [ '_T', '_T', '_T',
-			 			       '_T', '_T', 'EXHIB', '_T']
-
-				}
-			},
-			{
-				TopicName: "Cinema spending per person",
-			    id: 'GBO',
-			    rev: 2,
-			    Geo: {
-					location: 'Unesco'
-				 },
-
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-
-			        IndicatorPosition: 0,
-			        CountryPosition: 2,
-			        IndicatorName: 'UNIT_MEASURE',
-			        DisplayMessageOrder:[ 'Extra', 'Indicator'],
-			 		DisplayExtra: ' Cinema spending (local currency) ',
-			 		extras: [ '_T', '_T', '_T',
-			 			       '_T', '_T', '_T','EXHIB', '_T']
-
-				}
-			},
-
-			{
-				TopicName: "Attendance frequency",
-			    id: 'ATTEND_FREQ',
-			    rev: 1,
-			   	Geo: {
-					location: 'Unesco'
-				 },
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-
-			        IndicatorPosition: 0,
-			        CountryPosition: 2,
-			        DisplayMessageOrder:[ 'Extra'],
-			 		DisplayExtra: 'Attendance frequency per year',
-			        IndicatorName: 'UNIT_MEASURE',
-			 		extras: [ '_T', '_T', '_T',
-			 			       '_T', '_T', '_T', 'EXHIB', '_T']
-
-				}
-			},
-			{
-				TopicName: "Average ticket price",
-			    id: 'AVG_TICKET_PRICE',
-			    rev: 1,
-			    Geo: {
-					location: 'Unesco'
-				 },
-			    Time: {
-			     	Type: 'A',
-			     	Start: 2005,
-			      	End: 2015
-			      },
-			     UrlStructure:{
-			        orderOption: 1,
-			        DataBasePosition:0,
-
-			        IndicatorPosition: 0,
-			        CountryPosition: 2,
-			        DisplayMessageOrder:[ 'Extra', 'Indicator'],
-			 		DisplayExtra: 'Average ticket price in',
-			        IndicatorName: 'UNIT_MEASURE',
-			 		extras: [ '_T', '_T', '_T',
-			 			       '_T', '_T', '_T', 'EXHIB', '_T']
-
-				}
-			},
-   	               ]
-    }
-*/
 ]
 
 export default UnescoDatabase
