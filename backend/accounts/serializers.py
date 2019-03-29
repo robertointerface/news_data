d81f2015fec71fcdbc345f70217d4707ea464b5c
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-03-29 10:31:34 +0100

    Pylint accounts files and improve according to pylint suggestions

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,39 +1,39 @@
+"""
+    serializers - Serializers class used for the creating, retrieval, update and  delete of
+    objects on tables in 'accounts.models'
 
+    Members:
+        # UserSerializer - Serializer with public user information.
+        # UserInfoSerializer
+
+"""
 from rest_framework import serializers
-from rest_framework_jwt.settings import api_settings
-from .models import User, Follow
-from django.shortcuts import get_object_or_404
 from rest_framework.serializers import ValidationError
-from django.db.models import ObjectDoesNotExist
+from rest_framework_jwt.settings import api_settings
+from django.shortcuts import get_object_or_404
 from django.db import DatabaseError
-
-try:
-    from backend.backend.settings import Migration
-except:
-    from backend.settings import Migration
-
-if Migration:
-    from search_data.serializers import UserDataSerializers
-else:
-    from backend.search_data.serializers import UserDataSerializers
-    from backend.create_new.models import New
+from .models import User, Follow
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    """
+    standard User serializer
+    """
     class Meta:
+        """Serializer Meta"""
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'location', 'about_me')
 
 
 class UserInfoSerializer(serializers.ModelSerializer):
-    """serializer for user public information
-
+    """
+    serializer for fileds in User'  that are public information
     """
     user_created_new = serializers.SerializerMethodField()
     user_rel_followed = serializers.SerializerMethodField()
 
     class Meta:
+        """Serializer Meta"""
         model = User
         fields = ('username', 'about_me', 'location', 'user_created_new', 'user_rel_followed')
 
@@ -47,7 +47,9 @@ class UserInfoSerializer(serializers.ModelSerializer):
 
 
 class UserPrivateInfoSerializer(serializers.ModelSerializer):
-
+    """
+    Serializer use for the fields modifications on 'User', authentication required to access.
+    """
     user_rel_followed = serializers.SerializerMethodField()
     user_rel_follows = serializers.SerializerMethodField()
     user_saved_data = serializers.SerializerMethodField()
@@ -65,6 +67,15 @@ class UserPrivateInfoSerializer(serializers.ModelSerializer):
                   'user_saved_data')
 
     def update(self, instance, validated_data):
+        """
+        Update 'User' model
+        @param
+            instance - serializer instance
+            validated_data - data to update, validated accordingly to serializer.
+        @return
+            On Success - return none
+            On Failure - raise DatabaseError
+        """
         try:
             instance.first_name = validated_data['first_name']
             instance.location = validated_data['location']
@@ -75,21 +86,25 @@ class UserPrivateInfoSerializer(serializers.ModelSerializer):
             raise DatabaseError
 
     def get_user_created_new(self, obj):
+        """Get the count of news """
         return obj.user_created_new.count()
 
     def get_user_rel_followed(self, obj):
+        """Get the count of user_rel_followed """
         return obj.user_rel_followed.count()
 
     def get_user_rel_follows(self, obj):
+        """Get the count of user_rel_follows """
         return obj.user_rel_follows.count()
 
     def get_user_saved_data(self, obj):
+        """Get the count of user_saved_data """
         return obj.user_saved_data.count()
 
 
 class FollowSerializer(serializers.ModelSerializer):
     """
-    Serializer used to save following actions
+    Serializer used to create rows on table 'Follow'
     """
     class Meta:
         model = Follow
@@ -99,9 +114,18 @@ class FollowSerializer(serializers.ModelSerializer):
         return Follow.objects.create(**validated_data)
 
     def validate_followed(self, value):
+        """
+        Validate if user is already following that user.
+
+        @param
+            value - value to validate
+
+        @return
+            On success - return value
+            On failure - raise ValidationError
+        """
         follows_id = self.initial_data['follows']
         user = get_object_or_404(User, id=follows_id)
-        all_following = user.following.all()
         already_following = user.following.filter(id=value.id).first()
         if already_following is not None:
             raise ValidationError('already following')
@@ -112,6 +136,10 @@ class UserSerializerWithToken(serializers.ModelSerializer):
 
     token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('token', 'username', 'password')
 
     def get_token(self, obj):
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -129,7 +157,5 @@ class UserSerializerWithToken(serializers.ModelSerializer):
         instance.save()
         return instance
 
-    class Meta:
-        model = User
-        fields = ('token', 'username', 'password')
+
 
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,26 +1,24 @@
-from django.conf.urls import url, include
+"""
+Define urls for django.app accounts
 
-from django.conf import settings
-from django.conf.urls.static import static
+"""
+from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 import views
 
-urlpatterns = [
-            url(r'^token-auth/', obtain_jwt_token),
-            url(r'^token-refresh/', refresh_jwt_token),
-            url(r'^current_user/$', views.current_user),
-            url(r'^users/$', views.UserList.as_view()),
-            url(r'^signup/$', views.sign_up),
-            url(r'^verifyusertoken/(?P<token>[-\w]+)/', views.verify_token),
-            url(r'^edituserfirsttime/', views.edit_user_first_time),
-            url(r'^googleverify', views.google_signin),
-            url(r'^userpublicinfo$', views.UserPublicInfo.as_view()),
-            url(r'^userprivateinfo$', views.UserPrivateInfo.as_view()),
-            url(r'^follow$', views.SetUserFollow.as_view()),
-            url(r'^unfollow$', views.SetUserUnFollow.as_view()),
-            url(r'^isfollowing$', views.IsFollowing.as_view()),
-            url(r'^edituser$', views.EditUser.as_view()),
-            url(r'^changepassword', views.ChangePassword.as_view()),
-            url(r'^APIresetpassword', views.ResetPassword.as_view())
-
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [url(r'^token-auth/', obtain_jwt_token),
+               url(r'^token-refresh/', refresh_jwt_token),
+               url(r'^current_user/$', views.current_user),
+               url(r'^users/$', views.UserList.as_view()),
+               url(r'^signup/$', views.sign_up),
+               url(r'^verifyusertoken/(?P<token>[-\w]+)/', views.verify_token),
+               url(r'^edituserfirsttime/', views.edit_user_first_time),
+               url(r'^googleverify', views.google_signin),
+               url(r'^userpublicinfo$', views.UserPublicInfo.as_view()),
+               url(r'^userprivateinfo$', views.UserPrivateInfo.as_view()),
+               url(r'^follow$', views.SetUserFollow.as_view()),
+               url(r'^unfollow$', views.SetUserUnFollow.as_view()),
+               url(r'^isfollowing$', views.IsFollowing.as_view()),
+               url(r'^edituser$', views.EditUser.as_view()),
+               url(r'^changepassword', views.ChangePassword.as_view()),
+               url(r'^APIresetpassword', views.ResetPassword.as_view())]
