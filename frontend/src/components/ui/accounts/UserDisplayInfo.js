88a767156ff7b5d8468c4b4403905d6f3a906072
Roberto Alvarez <robertoalvarez@Robertos-iMac.local>
2019-04-06 13:32:31 +0200

    Fix bug, if user was logged and the user was not following the user, then the option of 'follow' was not displayed when it should be displayed

--- frontend/src/components/ui/accounts/UserDisplayInfo.js
+++ frontend/src/components/ui/accounts/UserDisplayInfo.js
@@ -16,26 +16,34 @@ const UserInfoCard = ({username='', location='', about_me='', followers=0, publi
      * that will be displayed is not the same as the one logged in, if is the same user then
      * no option of 'follow' is given as one can not follow himself*/
 
-    //var userSaved = JSON.parse(localStorage.getItem('user'));
-    var localUser = username
+    try{
+        /** try to get username if user is logged in*/
+        var userSaved = JSON.parse(localStorage.getItem('user'));
+        var localUser = userSaved['username']
+    }catch(errror){
+        /** if user is not logged in then just set it to empty*/
+        var localUser = '';
+    }
+
     return(
         <div className='card MB1'>
             <div className='row'>
                 <InfoCardItem fontIcon={faUser} text={`User: ${username}`}/>
-                {(localUser != username)?
-                    <div className='col-6'>
-                    {(canFollow) ?
-                        <div>
-                            {(following) ?
-                                <PrimaryButton message={'stop following'} onClick={(e) => onFollow(e, username)}/>
-                                : <PrimaryButton message={'follow'} onClick={(e) => onFollow(e, username)}/>
-                            }
-                        </div>
-                        :
-                        <p>Need to login to follow</p>
-                    }
-                    </div> : null
+                {((localUser) && (localUser != username))?
+                        <div className='col-6'>
+                        {(canFollow) ?
+                            <div>
+                                {(following) ?
+                                    <PrimaryButton message={'stop following'} onClick={(e) => onFollow(e, username)}/>
+                                    : <PrimaryButton message={'follow'} onClick={(e) => onFollow(e, username)}/>
+                                }
+                            </div>
+                            :
+                            <p>Need to login to follow</p>
+                        }
+                        </div> : null
                 }
+
                 <InfoCardItem fontIcon={faGlobeEurope} text={`Location: ${location}`}/>
                 <InfoCardItem fontIcon={faUsers} text={`Followers: ${followers}`}/>
                 <InfoCardItem fontIcon={faNewspaper} text={`Published news: ${publishedNews}`}/>
